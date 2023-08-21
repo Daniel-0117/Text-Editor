e5c489fb665b87db7e227550fb5b436db58ef84a
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-21 00:51:42 -0700

    finished databse and install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,38 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    
+    // Store the event for later use
+    window.deferredPrompt = event;
+
+    // Show the install button
+    butInstall.removeAttribute('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    // Check if there is a prompt event
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show the prompt
+    promptEvent.prompt();
+
+    // Wait for the user to respond to the prompt
+    const result = await promptEvent.userChoice;
+
+    // Hide the install button
+    butInstall.setAttribute('hidden', true);
+});
+    
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+
+    window.deferredPrompt = null;
+});
